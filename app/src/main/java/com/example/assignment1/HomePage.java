a189e5aacee15d4633c620017fa7315b16eb2b62
darsh1206 <darshpatelahm@gmail.com>
2024-03-13 21:10:29 -0400

    Implemented chained async tasks to take input of city names file from my github directory and show it in my dropdown menu

--- app/src/main/java/com/example/assignment1/HomePage.java
+++ app/src/main/java/com/example/assignment1/HomePage.java
@@ -1,17 +1,33 @@
 package com.example.assignment1;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.Scanner;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.annotation.Nullable;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class HomePage extends AppCompatActivity {
     public static final String TAG = HomePage.class.getSimpleName();
@@ -23,7 +39,9 @@ public class HomePage extends AppCompatActivity {
     private Button tripMaker;
     private int adultsNum =0;
     private int childrenNum=0;
+    private Spinner citySpinner;
 
+    @SuppressLint("MissingInflatedId")
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(TAG,"Application created");
@@ -35,6 +53,8 @@ public class HomePage extends AppCompatActivity {
         total = findViewById(R.id.total);
         error = findViewById(R.id.error);
         tripMaker = findViewById(R.id.tripPlanner);
+        citySpinner = findViewById(R.id.cities);
+        new FetchCitiesTask().execute();
 
         TextWatcher numAdder = new TextWatcher() {
             @Override
@@ -56,7 +76,6 @@ public class HomePage extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 personName = findViewById(R.id.name);
-
                 String[] nameParts = personName.getText().toString().split(" ");
                 checkForErrors(personName.getText().toString(), nameParts);
             }
@@ -109,4 +128,55 @@ public class HomePage extends AppCompatActivity {
 
         }
     }
+
+    private class FetchCitiesTask extends AsyncTask<Void, Void, List<String>> {
+        private final String CITIES_URL = "https://raw.githubusercontent.com/darsh1206/Cities/main/cities.json";
+
+        @Override
+        protected List<String> doInBackground(Void... voids) {
+            List<String> cityNames = new ArrayList<>();
+            HttpURLConnection connection = null;
+            try {
+                // connecting to an url of my github directory to find file of city names
+                URL url = new URL(CITIES_URL);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                Log.d(TAG, "Connection established");
+
+                InputStream in = new BufferedInputStream(connection.getInputStream());
+                String response = convertStreamToString(in);
+
+                JSONObject jsonObject = new JSONObject(response);
+                JSONArray citiesArray = jsonObject.getJSONArray("cities");
+                for (int i = 0; i < citiesArray.length(); i++) {
+                    cityNames.add(citiesArray.getString(i));
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+            return cityNames;
+        }
+
+        private String convertStreamToString(InputStream is) {
+            Scanner s = new Scanner(is).useDelimiter("\\A");
+            return s.hasNext() ? s.next() : "";
+        }
+
+        @Override
+        protected void onPostExecute(List<String> cityNames) {
+            super.onPostExecute(cityNames);
+
+            // creating an adapter to deploy cities from cities.json file into my dropdown menu
+            ArrayAdapter<String> adapter = new ArrayAdapter<>(HomePage.this,
+                    android.R.layout.simple_spinner_item, cityNames);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            citySpinner.setAdapter(adapter);
+            Log.d(TAG,"Added cities to dropdown menu");
+        }
+    }
+
 }
